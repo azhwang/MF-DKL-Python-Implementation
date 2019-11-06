908d1eb32056081ee6a23ae4634842be06e9baa6
Sihui Dai <sdai@caltech.edu>
2019-11-05 23:41:49 -0800

    Write sf_gp_opt and general algorithm

--- model.py
+++ model.py
@@ -4,12 +4,14 @@ import torch
 from gpytorch.kernels import RBFKernel, ScaleKernel
 from botorch.acquisition.analytic import UpperConfidenceBound
 from botorch.acquisition.analytic import ExpectedImprovement
+from botorch.optim import optimize_acqf
+
 
 class MultiFidelityModel():
     """A single task multi-fidelity GP model with abstract kernel.
     """
 
-    def __init__(self, num_fidel, total_budget, costs, kernel_name, acq_fn_name, beta=0.2, train_Xs=None, train_Ys=None, device='cpu'):
+    def __init__(self, num_fidel, total_budget, costs, kernel_name, acq_fn_name, bounds, num_candidates, num_restarts, raw_samples, beta=0.1, train_Xs=None, train_Ys=None, device='cpu'):
         '''Initializes a model for MF-MI-Greedy Baysian Optimization
         Args:
         - num_fidel (int): number of fidelity levels (including target)
@@ -21,6 +23,10 @@ class MultiFidelityModel():
         - train_Xs (list of Tensors): list of training input sets for each fidelity
         - train_Ys (list of Tensors): list of training output sets for each fidelity
         '''
+        self.bounds = bounds
+        self.num_candidates = num_candidates
+        self.num_restarts = num_restarts
+        self.raw_samples = raw_samples
         # initialize GP for target function
         kernels = {
             'RBF': ScaleKernel(RBFKernel()),
@@ -36,6 +42,8 @@ class MultiFidelityModel():
         self.costs = costs
         self.B = total_budget
         self.selected = set()
+        self.observed_y = set()
+        self.num_fidel = num_fidel
         # initialize GPs for each fidelity level
         self.e_ls = []
         if train_Xs is None:
@@ -51,12 +59,26 @@ class MultiFidelityModel():
         else:
             self.acq_fn = UpperConfidenceBound(self.f_m, beta)
         
+    def MI(self, y):
+        #TODO
+        pass
         
     def explore_lf(self):
         actions = []
-        action_costs = []
+        action_cost = 0
         threshold = 1/np.sqrt(self.B)
-        pass
+        # TODO
+        return set(), total_cost
 
     def sf_gp_opt(self):
-        pass
+        candidate, acq_value = optimize_acqf(self.acq_fn, bounds=self.bounds, q=self.num_candidates, num_restarts=self.num_restarts, raw_samples=self.raw_samples)
+        return torch.max(candidate)
+
+    def optimize(self):
+        while self.B > 0:
+            L, total_cost = self.explore_lf()
+            x = self.sf_gp_opt()
+            self.selected.update(L)
+            self.selected.add((x, self.num_fidel-1))
+            self.B -= total_cost + self.costs[-1]
+        return x #?
