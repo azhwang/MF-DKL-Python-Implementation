55574f48c1aa288f1a88de7a175347888fb309bb
Sihui Dai <sdai@caltech.edu>
2019-12-04 16:23:13 -0800

    fix bugs

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ def main():
     parser = argparse.ArgumentParser(description='MF-MI-Greedy')
     parser.add_argument('--budget', type=float, default=1000, help='budget')
     parser.add_argument('--num-fidelity', type=int, default=3, metavar='m', help='number of fidelities')
-    parser.add_argument('--fidelity-costs', type=list, default=[20, 5, 1], help='cost of each fidelity level')
+    parser.add_argument('--fidelity-costs', type=list, default=[9, 2.25, 1], help='cost of each fidelity level')
     parser.add_argument('--dataset', type=str, default='nanophotonics', choices=['nanophotonics', 'astronomy'],
                         help='dataset name')
+    parser.add_argument('--device', type=str, default='cpu', help='gpu')
     parser.add_argument('--kernel', type=str, default="RBF", choices=["RBF", "DKL", "Matern"], help='kernel to use')
     parser.add_argument('--acquisition-function', type=str, default="UCB", choices=["UCB", "EI"], help='acquisition function')
     args = parser.parse_args()
@@ -37,7 +38,7 @@ def main():
 
     if args.dataset == 'nanophotonics':
         model = MultiFidelityModel(args.num_fidelity, args.budget, args.fidelity_costs, args.kernel, (X, Y),
-                               args.acquisition_function, train_x, train_y, 5, is_discrete=True)
+                               args.acquisition_function, train_x, train_y, 5, is_discrete=True, device=args.device)
 
         best_x = model.optimize()
         print(best_x)
--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 from botorch.models.gp_regression import SingleTaskGP
 import numpy as np
 import torch
-import gpytorch
-from gpytorch.kernels import RBFKernel, ScaleKernel, AdditiveKernel
+from gpytorch.kernels import RBFKernel, ScaleKernel, MaternKernel, AdditiveKernel
 from botorch.acquisition.analytic import UpperConfidenceBound
 from botorch.acquisition.analytic import ExpectedImprovement
 from botorch.optim import optimize_acqf
 from botorch.acquisition import AcquisitionFunction
 from copy import deepcopy
+from botorch.fit import fit_gpytorch_model
+from gpytorch.mlls import ExactMarginalLogLikelihood
+
 import math
 
 class MultiFidelityModel():
@@ -15,7 +17,7 @@ class MultiFidelityModel():
     """
 
     def __init__(self, num_fidel, total_budget, costs, kernel_name, true_fns,
-        acq_fn_name, train_Xs, train_Ys, k, is_discrete=False, beta=0.1, device='cpu'):
+        acq_fn_name, train_Xs, train_Ys, k, is_discrete=False, device='cpu'):
         '''Initializes a model for MF-MI-Greedy Baysian Optimization
         Args:
         - num_fidel (int): number of fidelity levels (including target)
@@ -30,19 +32,25 @@ class MultiFidelityModel():
         - train_Ys (list of Tensors): list of training output sets for each 
             fidelity
         '''
+        self.device = device
         self.true_fns = true_fns
         self.k = k
         self.is_discrete = is_discrete
-        self.bounds = torch.stack([torch.zeros(train_Xs.size(1)), torch.ones(train_Xs.size(1))])
+        self.bounds = torch.stack([torch.zeros(train_Xs.size(1)).to(self.device),
+                                   torch.ones(train_Xs.size(1)).to(self.device)])
         # initialize GP for target function
         kernels = {
             'RBF': ScaleKernel(RBFKernel()),
-            'Matern': None # Botorch defaults to Matern
+            'Matern': ScaleKernel(MaternKernel())
         }
+        train_Xs = train_Xs.to(self.device)
+        train_Ys = train_Ys.to(self.device)
 
         self.f_m = SingleTaskGP(train_Xs, train_Ys[:, 0].unsqueeze(1), 
-                covar_module=kernels[kernel_name])
-        self.device = device
+                covar_module=kernels[kernel_name]).to(self.device)
+        mll = ExactMarginalLogLikelihood(self.f_m.likelihood, self.f_m)
+        fit_gpytorch_model(mll)
+
         self.costs = costs
         self.B = total_budget
         self.selected = set()
@@ -54,13 +62,14 @@ class MultiFidelityModel():
             e_l_covar = kernels[kernel_name]
             datax = torch.zeros(train_Xs.size())
             datay = torch.zeros(train_Ys[:, i].unsqueeze(1).size())
-            self.f_ls.append(SingleTaskGP(train_Xs, train_Ys[:, i].unsqueeze(1), covar_module=AdditiveKernel(self.f_m.covar_module, e_l_covar)))
+            gp = SingleTaskGP(train_Xs, train_Ys[:, i].unsqueeze(1),
+                            covar_module=AdditiveKernel(self.f_m.covar_module, e_l_covar)).to(self.device)
+            self.f_ls.append(gp)
+            mll = ExactMarginalLogLikelihood(gp.likelihood, gp)
+            fit_gpytorch_model(mll)
             self.e_ls.append(SingleTaskGP(datax, datay, 
-                    covar_module=e_l_covar))
-        if acq_fn_name == "EI":
-            self.acq_fn = ExpectedImprovement(self.f_m, -1000000)
-        else:
-            self.acq_fn = UpperConfidenceBound(self.f_m, beta)
+                    covar_module=e_l_covar).to(self.device))
+            self.acq_fn_name = acq_fn_name
 
     def information_gain_single_point(self, x, l):
         return torch.det(self.f_ls[l].covar_module(x, x).evaluate()) - torch.det(self.e_ls[l].covar_module(x, x).evaluate())
@@ -69,15 +78,16 @@ class MultiFidelityModel():
         return torch.det(self.f_m.covar_module(x, x).evaluate())
 
     def information_gain_set(self, x, l, L, f_ls, e_ls):
-        L.append((x, l))
-        cov_1 = self.covariance_across_levels(f_ls, L)
-        cov_2 = self.covariance_across_noise(e_ls, L)
+        L_copy = deepcopy(L)
+        L_copy.append((x, l))
+        cov_1 = self.covariance_across_levels(f_ls, L_copy)
+        cov_2 = self.covariance_across_noise(e_ls, L_copy)
         return torch.det(cov_1) - torch.det(cov_2)
 
     def covariance_across_levels(self, f_ls, points):
-        cov = torch.tensor([])
+        cov = torch.tensor([]).to(self.device)
         for i, (x1, l1) in enumerate(points):
-            cov_row = torch.tensor([])
+            cov_row = torch.tensor([]).to(self.device)
             for j, (x2, l2) in enumerate(points):
                 if l1 == l2:
                     cov_row = torch.cat([cov_row, f_ls[l1].covar_module(x1, x2).evaluate()])
@@ -88,14 +98,14 @@ class MultiFidelityModel():
         return cov
 
     def covariance_across_noise(self, e_ls, points):
-        cov = torch.tensor([])
+        cov = torch.tensor([]).to(self.device)
         for i, (x1, l1) in enumerate(points):
+            cov_row = torch.tensor([]).to(self.device)
             for j, (x2, l2) in enumerate(points):
-                cov_row = torch.tensor([])
                 if l1 == l2:
                     cov_row = torch.cat([cov_row, e_ls[l1].covar_module(x1, x2).evaluate()])
                 else:
-                    cov_row = torch.cat([cov_row, torch.zeros((len(x1), len(x2)))])
+                    cov_row = torch.cat([cov_row, torch.zeros((len(x1), len(x2))).to(self.device)])
             cov = torch.cat([cov, cov_row], dim=1)
         return cov
 
@@ -144,34 +154,46 @@ class MultiFidelityModel():
             else:
                 actions_fidel.append((x, l))
                 action_cost += self.costs[l]
+                # need to call my model first before I can update posterior
+                self.f_ls[l](x.unsqueeze(0))
                 if self.is_discrete:
-                    self.f_ls[l] = self.f_ls[l].condition_on_observations(x, Y[x_idx][l])
+                    self.f_ls[l] = self.f_ls[l].condition_on_observations(x.unsqueeze(0),
+                                                                          Y[x_idx][l].unsqueeze(0).unsqueeze(0))
                 else:
-                    self.f_ls[l] = self.f_ls[l].condition_on_observations(x, self.true_fns[l](x))
+                    self.f_ls[l] = self.f_ls[l].condition_on_observations(x.unsqueeze(0),
+                                                                          self.true_fns[l](x).unsqueeze(0).unsqueeze(0))
 
         return set(actions_fidel), action_cost
 
-    def sf_gp_opt(self):
+    def sf_gp_opt(self, best_f=-math.inf):
+        if self.acq_fn_name == "EI":
+            acq_fn = ExpectedImprovement(self.f_m, best_f)
+        else:
+            acq_fn = UpperConfidenceBound(self.f_m, 0.1)
         if self.is_discrete:
             X, _ = self.true_fns
-            acq_vals = self.acq_fn(X.unsqueeze(1))
+            acq_vals = acq_fn(X.unsqueeze(1))
             argmax = torch.argmax(acq_vals)
-            return X[argmax], argmax
+            max_val = torch.max(acq_vals)
+            return X[argmax], argmax, max_val
         else:
-            candidate, acq_value = optimize_acqf(self.acq_fn, bounds=self.bounds, q=1, num_restarts=5, raw_samples=20)
+            candidate, acq_value = optimize_acqf(acq_fn, bounds=self.bounds, q=1, num_restarts=5, raw_samples=20)
             acq_vals = acq_value.cpu().numpy()
             idx, = np.where(acq_vals == np.max(acq_vals))
             return candidate[idx]
 
     def optimize(self):
         print("beginning optimization")
+        print(self.selected)
+        max_val = -math.inf # max val of acq function seen so far
         while self.B > 0:
             print("exploring lf")
             L, total_cost = self.explore_lf()
             print("optimizing single fidelity")
-            x, x_idx = self.sf_gp_opt()
+            x, x_idx, max_val = self.sf_gp_opt(best_f=max_val)
             #total_queried = len(L) + 1
             self.selected.update(L)
+            print(self.selected)
             self.selected.add((x, 0))
             self.B -= total_cost + self.costs[0]
             print("number of queried: {}".format(len(self.selected)))
@@ -182,12 +204,15 @@ class MultiFidelityModel():
             print("updating posterior of f_m")
             if self.is_discrete:
                 _, Y = self.true_fns
-                print(Y[x_idx][0])
-                self.f_m = self.f_m.condition_on_observations(x, Y[x_idx][0])
+                self.f_m(x.unsqueeze(0))
+                self.f_m = self.f_m.condition_on_observations(x.unsqueeze(0),
+                                                              Y[x_idx][0].unsqueeze(0).unsqueeze(0))
             else:
-                self.f_m = self.f_m.condition_on_observations(x, self.true_fns[0](x))
+                self.f_m = self.f_m.condition_on_observations(x.unsqueeze(0),
+                                                              self.true_fns[0](x).unsqueeze(0).unsqueeze(0))
             
             print("current sf opt: {}".format(Y[x_idx][0]))
+            print("max: {}".format(torch.max(Y).item()))
             # update hyperparameters
             #if total_queried >= self.k:
                 # update hyperparameters
